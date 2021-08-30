6f278ca56e884e938aa5fe86951a4de65bdc4b32
Ruslan Zaremba <ruslanzaremba1990@gmail.com>
2021-08-30 10:05:09 +0300

    Change DEBUG in settings.py

--- stepic_tours/settings.py
+++ stepic_tours/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-67a36!54loqxj5mt5k92d!9z0)f@nl-zjup+v859kt(ms$ks%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
